79fa8d471e4b5f29f67ccffc5a87d03c850915aa
Tddhai123 <21020067@vnu.edu.vn>
2023-11-07 13:15:06 +0700

    Fix Header

--- src/botHeader/botHeader.js
+++ src/botHeader/botHeader.js
@@ -2,7 +2,13 @@ import './botHeader.css'
 import React, { useState } from 'react';
 
 function BotHeader() {
-  const [activeMenu, setActiveMenu] = useState(null);
+  const [activeMenu, setActiveMenu] = useState(false);
+  const [activeSidebar, setActiveSidebar] = useState(null);
+  const [activeSidebarMenu, setactiveSidebarMenu] = useState([]);
+
+  const handleActiveSidebar = () => {
+    setActiveSidebar(!activeSidebar)
+  }
 
   const handleMouseEnter = (menuIndex) => {
     setActiveMenu(menuIndex);
@@ -16,37 +22,122 @@ function BotHeader() {
     return activeMenu === menuIndex;
   };
 
+
+  const toggleSidebarMenu = (SidebarmenuIndex) => {
+    const updatedSidebarMenu = [...activeSidebarMenu];
+    if (updatedSidebarMenu.includes(SidebarmenuIndex)) {
+      // Nếu menuIndex đã có trong mảng, loại bỏ nó để đóng submenu.
+      updatedSidebarMenu.splice(updatedSidebarMenu.indexOf(SidebarmenuIndex), 1);
+    } else {
+      // Ngược lại, thêm menuIndex vào mảng để mở submenu.
+      updatedSidebarMenu.push(SidebarmenuIndex);
+    }
+    setactiveSidebarMenu(updatedSidebarMenu);
+  };
+
+  const isSidebarMenuOpen = (SidebarmenuIndex) => activeSidebarMenu.includes(SidebarmenuIndex);
+
+
   return (
     <div className="bottom-header">
+      <div id='sidebar-header' 
+        className={`sidebar-header ${activeSidebar === true? 'show' : ''}`}
+      >
+        <div id='content-sidebar' className='content-sidebar'>
+          <ul className='sidebar-menu'>
+            <li><a href='#'>Trang chủ</a></li>
+            <li
+              onClick={() => toggleSidebarMenu(0)}
+            >
+              <a className='content-sidebar-label' href='#'>
+                Dịch vụ
+                <i className={`arrow ${isSidebarMenuOpen(0) ? 'up' : 'down'}`}></i>
+              </a>
+              {isSidebarMenuOpen(0) &&
+                <div className='sidebar-sub-menu'>
+                  <div className='sidebar-sub-menu-content'>
+                    <a href="#">Chuyển phát hỏa tốc</a>
+                    <a href="#">Chuyển phát nhanh</a>
+                    <a href="#">Chuyển phát tiết kiệm</a>
+                    <a href="#">Dịch vụ đặc biệt</a>
+                    <a href="#">Dịch vụ trọn gói</a>
+                    <a href="#">Dịch vụ GTGT</a>
+                  </div>
+                </div>
+              }
+            </li>
+            <li
+              onClick={() => toggleSidebarMenu(1)}
+            >
+              <a className='content-sidebar-label' href='#'>
+                Tra cứu
+                <i className={`arrow ${isSidebarMenuOpen(1) ? 'up' : 'down'}`}></i>
+              </a>
+              {isSidebarMenuOpen(1) &&
+                <div className='sidebar-sub-menu'>
+                  <div className='sidebar-sub-menu-content'>
+                    <a href="#">Tra cứu bưu gửi</a>
+                    <a href="#">Ước tính cước vận chuyển</a>
+                    <a href="#">Thông tin đơn hàng</a>
+                  </div>
+                </div>
+              }
+            </li>
+            <li
+              onClick={() => toggleSidebarMenu(2)}
+            >
+              <a className='content-sidebar-label' href='#'>
+                Tin tức
+                <i className={`arrow ${isSidebarMenuOpen(2) ? 'up' : 'down'}`}></i>
+              </a>
+              {isSidebarMenuOpen(2) &&
+                <div className='sidebar-sub-menu'>
+                  <div className='sidebar-sub-menu-content'>
+                    <a href="#">Tin nội bộ</a>
+                    <a href="#">Tin dịch vụ mới</a>
+                    <a href="#">Tin ngành</a>
+                    <a href="#">Tin quốc tế</a>
+                    <a href="#">Thông tin hữu ích</a>
+                  </div>
+                </div>
+              }
+            </li>
+            <li><a href='#'>Giới thiệu</a></li>
+            <li><a href='#'>Liên hệ</a></li>
+          </ul>
+        </div>
+      </div>
       <div className="container-lg">
         <div className="wrapper-bottom-header">
-          <div className="left-header">
-            <a href="">
+         
+          <div className="bot-left-header">
+            <a href="#">
               <img className="no-select" src="./asset/Logo.png" alt="logo"></img>
             </a>
           </div>
           <div className="bot-right-header">
             <ul className="menu">
-              <li><a href=''>Trang chủ</a></li>
+              <li><a href="#">Trang chủ</a></li>
               <li
+                className='has-submenu'
                 onMouseEnter={() => handleMouseEnter(0)}
                 onMouseLeave={handleMouseLeave}
               >
-                <a href="/dich-vu-trong-nuoc/chuyen-phat-nhanh">
+                <a href="#">
                   Dịch vụ 
                   <i className={`arrow ${isMenuActive(0) ? 'up' : 'down'}`}></i>
                 </a>
                 {isMenuActive(0) && (
                   <div className="sub-menu">
-                    <div className="sub-menu-content d-flex justify-content-center">
+                    <div className="sub-menu-content">
                       <div className="sub-menu-item">
-                        <div className="sub-menu-item-list d-flex flex-column">
-                          <a href="/dich-vu-trong-nuoc/dich-vu-hoa-toc">Chuyển phát hỏa tốc</a>
-                          <a href="/dich-vu-trong-nuoc/chuyen-phat-nhanh">Chuyển phát nhanh</a>
-                          <a href="/dich-vu-trong-nuoc/chuyen-phat-tiet-kiem">Chuyển phát tiết kiệm</a>
-                          <a href="/dich-vu-trong-nuoc/dich-vu-dac-biet">Dịch vụ đặc biệt</a>
-                          <a href="/dich-vu-trong-nuoc/dich-vu-tron-goi">Dịch vụ trọn gói</a>
-                          <a href="/dich-vu-trong-nuoc/dich-vu-cong-them/dich-vu-cong-them-trong-nuoc">Dịch vụ GTGT</a>
+                        <div className="sub-menu-item-list">
+                          <a href="#">Chuyển phát hỏa tốc</a>
+                          <a href="#">Chuyển phát nhanh</a>
+                          <a href="#">Chuyển phát tiết kiệm</a>
+                          <a href="#">Dịch vụ đặc biệt</a>
+                          <a href="#">Dịch vụ trọn gói</a>
+                          <a href="#">Dịch vụ GTGT</a>
                         </div>
                       </div>
                     </div>
@@ -55,21 +146,22 @@ function BotHeader() {
               </li>
 
               <li 
+                className='has-submenu'
                 onMouseEnter={() => handleMouseEnter(1)}
                 onMouseLeave={handleMouseLeave}
               >
-                <a href="/tra-cuu">
+                <a href="#">
                   Tra cứu
                   <i className={`arrow ${isMenuActive(1) ? 'up' : 'down'}`}></i>
                 </a>
                 {isMenuActive(1) && (
                   <div className="sub-menu">
-                    <div className="sub-menu-content d-flex justify-content-center">
+                    <div className="sub-menu-content">
                       <div className="sub-menu-item">
-                        <div className="sub-menu-item-list d-flex flex-column">
-                          <a href="/tin-tuc/tra-cuu-buu-gui">Tra cứu bưu gửi</a>
-                          <a href="/tin-tuc/uoc-tinh-cuoc-van-chuyen">Ước tính cước vận chuyển</a>
-                          <a href="/tin-tuc/thong-tin-don-hang">Thông tin đơn hàng</a>
+                        <div className="sub-menu-item-list">
+                          <a href="#">Tra cứu bưu gửi</a>
+                          <a href="#">Ước tính cước vận chuyển</a>
+                          <a href="#">Thông tin đơn hàng</a>
                         </div>
                       </div>
                     </div>
@@ -78,23 +170,24 @@ function BotHeader() {
               </li>
 
               <li 
+                className='has-submenu'
                 onMouseEnter={() => handleMouseEnter(2)}
                 onMouseLeave={handleMouseLeave}
               >
-                <a href="/tin-tuc">
+                <a href="#">
                   Tin tức 
                   <i className={`arrow ${isMenuActive(2) ? 'up' : 'down'}`}></i>
                 </a>
                 {isMenuActive(2) && (
                   <div className="sub-menu">
-                    <div className="sub-menu-content d-flex justify-content-center">
+                    <div className="sub-menu-content">
                       <div className="sub-menu-item">
-                        <div className="sub-menu-item-list d-flex flex-column">
-                          <a href="/tin-tuc/tin-noi-bo">Tin nội bộ</a>
-                          <a href="/tin-tuc/tin-dich-vu-moi">Tin dịch vụ mới</a>
-                          <a href="/tin-tuc/tin-nganh">Tin ngành</a>
-                          <a href="/tin-tuc/tin-quoc-te">Tin quốc tế</a>
-                          <a href="/tin-tuc/thong-tin-huu-ich">Thông tin hữu ích</a>
+                        <div className="sub-menu-item-list">
+                          <a href="#">Tin nội bộ</a>
+                          <a href="#">Tin dịch vụ mới</a>
+                          <a href="#">Tin ngành</a>
+                          <a href="#">Tin quốc tế</a>
+                          <a href="#">Thông tin hữu ích</a>
                         </div>
                       </div>
                     </div>
@@ -102,17 +195,39 @@ function BotHeader() {
                 )}
               </li>
 
-              <li><a href=''>Giới Thiệu</a></li>
-              <li><a href=''>Liên hệ</a></li>
+              <li><a href='#'>Giới thiệu</a></li>
+              <li><a href='#'>Liên hệ</a></li>
             </ul>
           </div>
-          <div className='header-search'>
-            <a href="/tim-kiem">
+          <div 
+          class="menu-button-show d-lg-none"
+          onClick={handleActiveSidebar}
+         >
+            <span>
               <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13.3328 13.7294L10.6211 10.8799C11.3183 10.0425 11.7003 8.98884 11.7003 7.89196C11.7003 5.32919 9.63663 3.24414 7.10015 3.24414C4.56367 3.24414 2.5 5.32919 2.5 7.89196C2.5 10.4547 4.56367 12.5398 7.10015 12.5398C8.05238 12.5398 8.95981 12.2496 9.73563 11.6987L12.4679 14.5699C12.5821 14.6897 12.7357 14.7558 12.9003 14.7558C13.0561 14.7558 13.2039 14.6958 13.3162 14.5866C13.5546 14.3548 13.5622 13.9705 13.3328 13.7294ZM7.10015 4.45661C8.97501 4.45661 10.5003 5.99767 10.5003 7.89196C10.5003 9.78624 8.97501 11.3273 7.10015 11.3273C5.22529 11.3273 3.70004 9.78624 3.70004 7.89196C3.70004 5.99767 5.22529 4.45661 7.10015 4.45661Z" fill="#313131">
+                <path 
+                  d="M14.2209 14.1158H1.77648C1.6586 14.1158 1.54556 14.0668 1.46221 13.9796C1.37886 13.8923 
+                  1.33203 13.774 1.33203 13.6507C1.33203 13.5273 1.37886 13.409 1.46221 13.3218C1.54556 13.2345 
+                  1.6586 13.1855 1.77648 13.1855H14.2209C14.3388 13.1855 14.4518 13.2345 14.5352 13.3218C14.6185 
+                  13.409 14.6654 13.5273 14.6654 13.6507C14.6654 13.774 14.6185 13.8923 14.5352 13.9796C14.4518 
+                  14.0668 14.3388 14.1158 14.2209 14.1158Z" fill="#313131">
+                </path>
+                <path 
+                  d="M14.2209 9.46344H1.77648C1.6586 9.46344 1.54556 9.41443 1.46221 9.32721C1.37886 9.23998 
+                  1.33203 9.12168 1.33203 8.99832C1.33203 8.87496 1.37886 8.75666 1.46221 8.66943C1.54556 8.58221 
+                  1.6586 8.5332 1.77648 8.5332H14.2209C14.3388 8.5332 14.4518 8.58221 14.5352 8.66943C14.6185 8.75666 
+                  14.6654 8.87496 14.6654 8.99832C14.6654 9.12168 14.6185 9.23998 14.5352 9.32721C14.4518 9.41443 
+                  14.3388 9.46344 14.2209 9.46344Z" fill="#313131">
+                </path>
+                <path 
+                  d="M14.2209 4.81304H1.77648C1.6586 4.81304 1.54556 4.76404 1.46221 4.67682C1.37886 4.58959 
+                  1.33203 4.47129 1.33203 4.34793C1.33203 4.22457 1.37886 4.10627 1.46221 4.01904C1.54556 3.93182 
+                  1.6586 3.88281 1.77648 3.88281H14.2209C14.3388 3.88281 14.4518 3.93182 14.5352 4.01904C14.6185 
+                  4.10627 14.6654 4.22457 14.6654 4.34793C14.6654 4.47129 14.6185 4.58959 14.5352 4.67682C14.4518 
+                  4.76404 14.3388 4.81304 14.2209 4.81304Z" fill="#313131">
                 </path>
               </svg>
-            </a>
+            </span>
           </div>
         </div>
       </div>
--- src/topHeader/topHeader.js
+++ src/topHeader/topHeader.js
@@ -7,17 +7,17 @@ function TopHeader() {
     <div className="top-header">
       <div className="container-lg">
         <div className="wrapper-top-header">
-          <div className="left-header">
-            <span className="left-header__label">Hotline:&nbsp;</span>
+          <div className="top-left-header">
+            <span className="top-left-header__label">Hotline:&nbsp;</span>
             <span className="hotline1">1900 0000</span>
           </div>
-          <div className="right-header">
-            <div className="right-header__label">Ngôn ngữ:&nbsp;</div>
+          <div className="top-right-header">
+            <div className="top-right-header__label">Ngôn ngữ:&nbsp;</div>
             <div className="language">
                 Tiếng Việt
             </div>
             <div className="login">
-              <a href="">Đăng nhập</a>
+              <a href="#">Đăng nhập</a>
             </div>
           </div>
         </div>
