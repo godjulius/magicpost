b5db3d9eabe98230910f74299a84dc21f435f718
duyhehehe <duy20036athcsns@gmail.com>
2023-11-15 09:53:32 +0700

    Create EmployeeController

--- frontend/src/CreateAccount/createAccount.js
+++ frontend/src/CreateAccount/createAccount.js
@@ -1,4 +1,7 @@
 function CreateAccount() {
+
+
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
