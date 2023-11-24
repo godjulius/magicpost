362d02d909754e56ac2cd0dc526c0f0c9b4a38c1
Tddhai123 <21020067@vnu.edu.vn>
2023-11-25 01:01:11 +0700

    update AccountManagement.js

--- frontend/src/admin/AccountManagement.js
+++ frontend/src/admin/AccountManagement.js
@@ -151,7 +151,7 @@ const AccountManagement = () => {
               {isLoadingEmployeesList || (
                 <tbody>
                   {currentEmployees.map((employee) => (
-                    <tr key={employee.id}>
+                    <tr key={employee.employee_id}>
                       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                         <p className="text-gray-900 whitespace-no-wrap">
                           {employee.employee_id}
